9e28e38946070cf0da20733ae210a4804fabe29c
Severin Ryberg <s.ryberg@dras.tech>
2023-11-01 12:16:20 +0000

    make create_memo work

--- Readme.md
+++ Readme.md
@@ -19,26 +19,3 @@ After cloning this repo, run me with:
 ```bash
 trunk serve
 ```
-
-## The Problem
-
-Since logging-in requires talking to a backend server in an async function call, the Control-Area
-component must make use of the `create_local_resource` functionality in Leptos. However I could
-not find an obvious (at least, to me, with my limited knowledge of Leptos and websassembly as a
-whole) way to set the result of this async function call into the App's `user_data` context.
-
-The only thing which seemed to work involved using the `create_effect` functionality in Leptos to
-invoke the `user_data.set()` setter. However, this goes directly against what is stated in the
-[Leptos docs](https://docs.rs/leptos/latest/leptos/fn.create_effect.html):
-
-> Effects are intended to run side-effects of the system, not to synchronize state within the
-> system. In other words: don’t write to signals within effects, unless you’re coordinating with
-> some other non-reactive side effect. (If you need to define a signal that depends on the value
-> of other signals, use a derived signal or create_memo).
-
-Unfortunately, using a derived signal or create_memo did not seem to work, either.
-
-## The Question
-
-How would one get around having to use `create_effect`, here? Alternatively, how else would one
-implement reactive user-logins resulting from async server communication.
--- src/components/control_area.rs
+++ src/components/control_area.rs
@@ -13,7 +13,7 @@ use crate::util::{
 // also implemented a view generator function for each case.
 #[derive(PartialEq, Clone)]
 pub enum LoginAttemptStatus {
-    None,
+    Initial,
     NoUser,
     NeedsLogin,
     NeedsLogout,
@@ -29,7 +29,7 @@ impl std::fmt::Display for LoginAttemptStatus {
             LoginAttemptStatus::NeedsLogout => write!(f, "Waiting to trigger logout..."),
             LoginAttemptStatus::Failed(err) => write!(f, "Failed with '{}'", err),
             LoginAttemptStatus::Succeeded(user) => write!(f, "Succeeded for '{}'", user.username),
-            LoginAttemptStatus::None => write!(f, "No status? But why!?"),
+            LoginAttemptStatus::Initial => write!(f, "Initial status"),
         }
     }
 }
@@ -42,7 +42,7 @@ impl LoginAttemptStatus {
             LoginAttemptStatus::NeedsLogout => self.make_view_needs_logout(),
             LoginAttemptStatus::Failed(_) => self.make_view_failed(setter),
             LoginAttemptStatus::Succeeded(_) => self.make_view_succeeded(setter),
-            LoginAttemptStatus::None => self.make_view_none(setter),
+            LoginAttemptStatus::Initial => self.make_view_none(setter),
         }
     }
 
@@ -130,12 +130,42 @@ pub fn ControlArea() -> impl IntoView {
         .expect("user-data setting context should be available");
 
     // Create a local signal which keeps track of login attempts (i.e. success, failure, in-progress, ect..)
-    let (login_tracker, set_login_tracker) = create_signal(LoginAttemptStatus::NoUser);
+    let (login_tracker, set_login_tracker) = create_signal(LoginAttemptStatus::Initial);
 
     // Define a function which can handle Login attempts. Since this potentially talks to a backend
     // server, it will need to be wrapped in a 'create_local_resource'
     let try_next_login_attempt = |tracker: LoginAttemptStatus| async move {
         match tracker {
+            // On the initial load, we'll check if a cookie exists. If so, we'll try loading user data
+            LoginAttemptStatus::Initial => {
+                console_log_str("Reading cookies".to_string());
+                match search_for_cookie(AUTH_TOKEN_COOKIE_NAME) {
+                    Ok(search_result) => {
+                        console_log_str("Found cookie".to_string());
+                        match search_result {
+                            Some(_) => match echo_user().await {
+                                Ok(response) => match response {
+                                    Some(user_data) => LoginAttemptStatus::Succeeded(user_data),
+                                    None => LoginAttemptStatus::NoUser,
+                                },
+                                Err(err) => {
+                                    console_log_str(format!(
+                                        "Error pinging user data -- {}",
+                                        err.to_string()
+                                    ));
+                                    LoginAttemptStatus::NoUser
+                                }
+                            },
+                            None => LoginAttemptStatus::NoUser,
+                        }
+                    }
+                    Err(err) => {
+                        console_log_str(format!("Error reading cookies -- {}", err.to_string()));
+                        return LoginAttemptStatus::NoUser;
+                    }
+                }
+            }
+            // If a LogIn action is requested, then we'll call the login function, and handle it's result
             LoginAttemptStatus::NeedsLogin => {
                 let login_attempt_result = login().await;
 
@@ -144,6 +174,7 @@ pub fn ControlArea() -> impl IntoView {
                     Err(err) => LoginAttemptStatus::Failed(err.to_string()),
                 }
             }
+            // If a Logout action is requested, then we'll call the logout function, and handle it's result
             LoginAttemptStatus::NeedsLogout => match logout().await {
                 Ok(_) => LoginAttemptStatus::NoUser,
                 Err(err) => {
@@ -151,82 +182,26 @@ pub fn ControlArea() -> impl IntoView {
                     LoginAttemptStatus::NoUser
                 }
             },
+            // Any other status is passed on as-is
             other => other,
         }
     };
 
     let login_action = create_local_resource(move || login_tracker.get(), try_next_login_attempt);
-
-    // In order to link the internal signal 'login_tracker' to the parent App's 'user-data' context, I need a way to
-    // update the latter whenever the former changes. The only way I can get this to work is to use a `create_effect`.
-    // However this is specifically AGAINST the create_effect docs, which state that one should not use create_effect
-    // to perform any sort of setting within the Leptos reactive system. The docs suggest using a memo, or a derived
-    // signal, but nether seem to work here
-    create_effect(move |_| {
-        let _login_action = login_action.get().unwrap_or(LoginAttemptStatus::None);
-        console_log_str(format!("Calling login tracker effect -- {}", _login_action));
-        match _login_action {
+    let handle_login_action = create_memo(move |_| {
+        let login_action = login_action.get().unwrap_or(LoginAttemptStatus::Initial);
+        console_log_str(format!("Calling login tracker memo -- {}", login_action));
+        match login_action.clone() {
             LoginAttemptStatus::Succeeded(user_data) => set_user_data.set(Some(user_data)),
             _ => set_user_data.set(None),
-        }
-    });
-
-    // Additionally, I want to check if there is already a logged-in user from a previous session. If so, I need to
-    // update the login-tracker signal. This requires talking with the backend server, and so must be async (hence
-    // another create_local_resource). Again, to link this back into the reactive system, I could only get it to
-    // work with another create_effect
-    let cached_user_data = create_local_resource(
-        || (),
-        |_| async move {
-            console_log_str("Reading cookies".to_string());
-            let auth_cookie = match search_for_cookie(AUTH_TOKEN_COOKIE_NAME) {
-                Ok(search_result) => {
-                    console_log_str("Found cookie".to_string());
-                    search_result
-                }
-                Err(err) => {
-                    console_log_str(format!("Error reading cookies -- {}", err.to_string()));
-                    None
-                }
-            };
-
-            let user_data = match auth_cookie {
-                Some(_) => match echo_user().await {
-                    Ok(val) => val,
-                    Err(err) => {
-                        console_log_str(format!("Error pinging user data -- {}", err.to_string()));
-                        None
-                    }
-                },
-                None => None,
-            };
-
-            user_data
-        },
-    );
-    create_effect(move |_| match cached_user_data.get() {
-        Some(user_data) => {
-            match user_data.clone() {
-                Some(val) => {
-                    console_log_str(format!(
-                        "Setting user from previous session -- {}",
-                        val.username
-                    ));
-                    set_login_tracker.set(LoginAttemptStatus::Succeeded(val));
-                }
-                None => console_log_str(format!("No user from previous session")),
-            };
-        }
-        None => console_log_str(format!("User data cache has not finished loading")),
+        };
+        login_action
     });
 
     // View generation
     view! {
         <div id="control-area">
-        {move || match login_action.get(){
-            Some(status) => status.make_view(set_login_tracker),
-            None => LoginAttemptStatus::None.make_view(set_login_tracker),
-        }}
+            {move || handle_login_action.get().make_view(set_login_tracker)}
         </div>
     }
 }
